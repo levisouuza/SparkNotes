65c41d6bb922d63ad53681b27b55fcd63ac760f9
Levi Souza <62394086+levisouuza@users.noreply.github.com>
2022-04-29 14:40:29 -0300

    Update SparkLoadFile.py

--- SparkLoadFile.py
+++ SparkLoadFile.py
@@ -2,22 +2,27 @@
 
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName('LoadFile').getOrCreate()
+def main():
+  spark = SparkSession.builder.appName('LoadFile').getOrCreate()
 
-file_location = "/FileStore/tables/cruise_ship_info.csv"
-file_type = "csv"
+  file_location = "/FileStore/tables/cruise_ship_info.csv"
+  file_type = "csv"
 
-# CSV options
-infer_schema = "True"
-first_row_is_header = "True"
-delimiter = ","
+  # CSV options
+  infer_schema = "True"
+  first_row_is_header = "True"
+  delimiter = ","
 
-# The applied options are for CSV files. For other file types, these will be ignored.
-df = spark.read.format(file_type) \
-  .option("inferSchema", infer_schema) \
-  .option("header", first_row_is_header) \
-  .option("sep", delimiter) \
-  .load(file_location)
+  # The applied options are for CSV files. For other file types, these will be ignored.
+  df = spark.read.format(file_type) \
+    .option("inferSchema", infer_schema) \
+    .option("header", first_row_is_header) \
+    .option("sep", delimiter) \
+    .load(file_location)
+  
+  return df
 
-# Show dataframe
-df.show()
+  # Show dataframe
+  
+if __name__ == 'main':
+  main()
